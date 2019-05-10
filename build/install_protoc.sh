554c65f41ed67282ab17b1b41fc5cfaa6355cec4
Benjamin Eder <barny.eder@gmail.com>
2019-05-10 19:42:14 +0200

    Potential jenkins CI fix

--- build/install_protoc.sh
+++ build/install_protoc.sh
@@ -2,7 +2,7 @@
 
 PROTOBUF_VERSION=3.7.1
 PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-if [[ ! (-e ${PROTOC_FILENAME}) ]]
+if [[ ! -e ${PROTOC_FILENAME} ]]
 then
     wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
     unzip -o ${PROTOC_FILENAME}
