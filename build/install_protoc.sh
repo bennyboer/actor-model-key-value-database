ec69109bdce34d676d8fe27454960b62c273b5fa
Benjamin Eder <barny.eder@gmail.com>
2019-05-10 19:25:27 +0200

    Potential jenkins CI fix

--- build/install_protoc.sh
+++ build/install_protoc.sh
@@ -5,6 +5,7 @@ PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
 unzip ${PROTOC_FILENAME}
 bin/protoc --version
+ls
 
 # Add to path
 PROTOC_BINARY_PATH=$(realpath bin/protoc)
