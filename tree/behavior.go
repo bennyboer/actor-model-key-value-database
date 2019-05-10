d24b5ff047b2ea1556fe8819e19895e9e5b287e3
Benjamin Eder <barny.eder@gmail.com>
2019-05-10 22:08:43 +0200

    Fixed bug where request messages could get lost

--- tree/behavior.go
+++ tree/behavior.go
@@ -1,6 +1,7 @@
 package tree
 
 import (
+	"errors"
 	"github.com/AsynkronIT/protoactor-go/actor"
 	"github.com/ob-vss-ss19/blatt-3-sudo/messages"
 	"log"
@@ -21,20 +22,30 @@ func NewNode(capacity int, values *Storage) actor.Actor {
 	return act
 }
 
-func (n *Node) forwardKeyedMessage(context actor.Context, key int32) {
+func (n *Node) forwardKeyedMessage(context actor.Context, key int32) error {
 	var address *actor.PID = nil
-	if n.searchkey <= key {
-		if len(context.Children()) > 1 {
-			address = context.Children()[1]
-		}
-	} else {
+
+	if key <= n.searchkey {
 		if len(context.Children()) > 0 {
 			address = context.Children()[0]
+		} else {
+			return errors.New("cannot find left child")
+		}
+	} else {
+		if len(context.Children()) > 1 {
+			address = context.Children()[1]
+		} else {
+			return errors.New("cannot find right child")
 		}
 	}
-	if address != nil {
-		context.Forward(address)
+
+	if address == nil {
+		return errors.New("could not forward message to child")
 	}
+
+	context.Forward(address)
+
+	return nil
 }
 
 func (n *Node) LeafBehavior(context actor.Context) {
@@ -55,7 +66,6 @@ func (n *Node) LeafBehavior(context actor.Context) {
 			context.Respond(&messages.RemoveResponse{Success: false, RemovedPair: nil})
 		}
 	case *messages.InsertRequest:
-		log.Println("Insert into leaf")
 		if len(*n.values) <= n.capacity {
 			(*n.values)[msg.Entry.Key] = msg.Entry.Value
 			log.Printf("[Node] Inserted %d with %v", msg.Entry.Key, msg.Entry.Value)
@@ -111,7 +121,6 @@ func (n *Node) LeafBehavior(context actor.Context) {
 		}
 		context.Respond(&messages.InsertResponse{Success: true})
 	case *messages.TraverseRequest:
-		log.Println("Traverse leaf")
 		var pairs = make([]*messages.KeyValuePair, 0, len(*n.values))
 
 		for k, v := range *(n.values) {
@@ -134,8 +143,13 @@ func (n *Node) LeafBehavior(context actor.Context) {
 func (n *Node) NodeBehavior(context actor.Context) {
 	switch msg := context.Message().(type) {
 	case *messages.SearchRequest:
-		n.forwardKeyedMessage(context, msg.Key)
-
+		err := n.forwardKeyedMessage(context, msg.Key)
+		if err != nil {
+			context.Respond(&messages.SearchResponse{
+				Success: false,
+				Entry:   nil,
+			})
+		}
 	case *messages.InsertRequest:
 		println("Insert into node")
 		var address *actor.PID
@@ -160,17 +174,21 @@ func (n *Node) NodeBehavior(context actor.Context) {
 		}
 		context.Forward(address)
 	case *messages.RemoveRequest:
-		n.forwardKeyedMessage(context, msg.Key)
+		err := n.forwardKeyedMessage(context, msg.Key)
+		if err != nil {
+			context.Respond(&messages.RemoveResponse{
+				Success:     false,
+				RemovedPair: nil,
+			})
+		}
 	case *messages.DeleteTreeRequest:
 		for _, child := range context.Children() {
 			context.Send(child, &messages.DeleteTreeRequest{})
 			child.Poison()
 		}
 	case *messages.TraverseRequest:
-		log.Println("Traverse Node")
 		pairs := make([]*messages.KeyValuePair, 0)
 		for _, child := range context.Children() {
-			log.Println("Traverse next Node")
 			result, _ := context.RequestFuture(child, &messages.TraverseRequest{}, TIMEOUT).Result()
 			response, err := result.(*messages.TraverseResponse)
 
